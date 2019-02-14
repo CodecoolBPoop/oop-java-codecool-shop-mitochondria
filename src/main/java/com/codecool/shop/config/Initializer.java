db2cde11b730726b772c8c02ee7faa4fefaae24a
Péter Pál Vanyó <vanyo.peter.pal@gmail.com>
2019-02-14 12:46:16 +0100

    the two separated filter has been finished

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -37,9 +37,9 @@ public class Initializer implements ServletContextListener {
 
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.", 1);
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
-        ProductCategory accessories = new ProductCategory("Accessories", "Video Game", "All accessories you would need for your gaming experience.", 2 );
+        ProductCategory accessories = new ProductCategory("Accessories", "Video Game", "All accessories you would need for your gaming experience.");
         productCategoryDataStore.add(accessories);
 
 
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
+
     ProductDao productDataStore = ProductDaoMem.getInstance();
     ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
     SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
@@ -43,7 +44,8 @@ public class ProductController extends HttpServlet {
         context.setVariable("products", productDataStore.getAll());
         context.setVariable("supplier", supplierDataStore.getAll());
         context.setVariable("shoppingcart", ShoppingCart.getAll());
-        context.setVariable("all", "All Category");
+        context.setVariable("all", "All Categories");
+        context.setVariable("all2", "All Suppliers");
         engine.process("product/index.html", context, resp.getWriter());
     }
 
@@ -52,22 +54,42 @@ public class ProductController extends HttpServlet {
 
             throws ServletException, IOException {
 
+
+
         String testText = req.getParameter("testName");
+        String testText2 = req.getParameter("testName2");
 
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        if (testText.equalsIgnoreCase("All Category")) {
+
+
+        if (testText.equalsIgnoreCase("All Categories") && testText2.equalsIgnoreCase("All Suppliers")) {
             context.setVariable("products", productDataStore.getAll());
-        } else {
-            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.findByName(testText)));
         }
+        else if (testText2.equalsIgnoreCase("All Suppliers") && !testText.equalsIgnoreCase("All Categories")) {
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.findByName(testText)));
+        } else {
+            context.setVariable("products", productDataStore.getBy(supplierDataStore.findByName(testText2)));
+            System.out.println("intext2");
+        }
+
+
+//        if (testText2.equalsIgnoreCase("All Supplier")) {
+//            context.setVariable("products", productDataStore.getAll());
+//            System.out.println("all supplier");
+//        } else {
+//            context.setVariable("products", productDataStore.getAll());
+//            System.out.println("not all supplier");
+//        }
+
 
         context.setVariable("category", productCategoryDataStore.getAll());
         context.setVariable("supplier", supplierDataStore.getAll());
         context.setVariable("shoppingcart", ShoppingCart.getAll());
-        context.setVariable("all", "All Category");
+        context.setVariable("all", "All Categories");
+        context.setVariable("all2", "All Suppliers");
         engine.process("product/index.html", context, resp.getWriter());
     }
 
