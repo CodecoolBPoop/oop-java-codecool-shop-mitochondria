c518034b454326df81e4548b982c55bbdfa94d1c
milanszakacs <milan.szakacs@gmail.com>
2019-02-14 15:40:53 +0100

    Currency added to prices

--- src/main/java/com/codecool/shop/controller/AddToCartController.java
+++ src/main/java/com/codecool/shop/controller/AddToCartController.java
@@ -34,7 +34,7 @@ public class AddToCartController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < ProductDaoMem.getInstance().getAll().size(); i++) {
 
             String str = Integer.toString(i);
 
