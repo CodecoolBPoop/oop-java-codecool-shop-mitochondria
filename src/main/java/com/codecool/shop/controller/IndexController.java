5ced325e705dd03f8cb5f28d56e7c0008a9a0491
David Belme <belmedavid@gmail.com>
2019-02-14 11:32:59 +0100

    The menu elements leading to various sites, css refactor, added index page, and product page.

--- src/main/java/com/codecool/shop/controller/IndexController.java
+++ src/main/java/com/codecool/shop/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.codecool.shop.controller;
 
+
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
@@ -21,19 +22,19 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(urlPatterns = {"/products"})
-public class ProductController extends HttpServlet {
+@WebServlet(urlPatterns = {"/"})
+public class IndexController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 
             throws ServletException, IOException {
 
-        ProductDao productDataStore = ProductDaoMem.getInstance();
+ /*       ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
         ShoppingCart shoppingCart = ShoppingCart.getInstance();
-
+*/
 //        Map params = new HashMap<>();
 //        params.put("category", productCategoryDataStore.find(1));
 //        params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
@@ -41,13 +42,13 @@ public class ProductController extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 //        context.setVariables(params);
-        context.setVariable("recipient", "World");
+    /*    context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.getAll());
         context.setVariable("products", productDataStore.getAll());
         context.setVariable("supplier", supplierDataStore.getAll());
         context.setVariable("shoppingcart", shoppingCart.getAll());
-        context.setVariable("all", "All Category");
-        engine.process("product/products.html", context, resp.getWriter());
+        context.setVariable("all", "All Category");*/
+        engine.process("product/index.html", context, resp.getWriter());
     }
 
 }
