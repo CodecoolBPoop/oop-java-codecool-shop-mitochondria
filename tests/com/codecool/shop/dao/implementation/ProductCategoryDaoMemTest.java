e6f8bea56c4554740a563e66ac2d3accafe44eec
Anna Laszlo <dewspider@gmail.com>
2019-02-27 10:27:00 +0100

    created DAOmem test cases not yet able to run

---
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoMemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
---
+++ tests/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoMemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void getBy1() {
+    }
+}
