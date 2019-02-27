3c5b28077600b565295bad220bb74f0e977818b3
milanszakacs <milan.szakacs@gmail.com>
2019-02-27 10:34:57 +0100

    ProductDao JDBC added

---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -0,0 +1,58 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDaoJDBC implements ProductDao {
+
+    private List<Product> data = new ArrayList<>();
+    private static ProductDaoJDBC instance = null;
+
+    /* A private Constructor prevents any other class from instantiating.
+     */
+    private ProductDaoJDBC() {
+    }
+
+    public static ProductDaoJDBC getInstance() {
+        if (instance == null) {
+            instance = new ProductDaoJDBC();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+}
